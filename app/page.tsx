fa84a45a8301b940e894b7da3df5c0298148edb8
trio-habel-jonathan <clodsertansu2@gmail.com>
2026-01-17 14:39:46 +0700

    upade ui

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default async function Home() {
     <div className="home-page">
         <div className="text-center mt-15 md:mt-25">
           <p className="text-title font-lexend-deca mx-auto max-w-4xl text-6xl text-black md:text-7xl animate-fade-in">
-            Everything You Need, <span className="text-orange-500">In One Place</span>
+            Everything You Need, <span className="text-[#A8DF8E]">In One Place</span>
           </p>
           <p className="text-sub mt-[40px] font-lexend-deca mx-auto max-w-3xl text-xl text-[#1e293b]">Explore a variety of products with a smooth and simple shopping experience, all in one place</p>
           <div className="button mt-[40px] flex-gap justify-center">
-            <Link href="/" target="blank" className="inline-flex items-center gap-2 px-12 py-5 rounded-4xl bg-orange-500">
+            <Link href="/" target="blank" className="inline-flex items-center gap-2 px-12 py-5 rounded-4xl bg-[#A8DF8E]">
               <p className="font-lexend-deca text-white text-[20px]">Explore</p>
               <svg className="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M20 12L14 6M20 12L14 18" stroke="#fff5f5" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
             </Link>
@@ -31,64 +31,60 @@ export default async function Home() {
           </div>
         </div>
         <div className="w-[85%] mx-auto mt-10">
-          <div className="mb-6">
-            <h2 className="text-xl text-center font-lexend-deca">Featured Products</h2>
-            <Link href="/" className="invisible md:visible font-lexend-deca">Explore More</Link>
+          <div className="mb-10 flex items-center justify-between">
+            <div>
+              <h2 className="text-xl font-lexend-deca">Featured Products</h2>
+              <p className="text-gray-500 font-lexend-deca mt-2">Discover our handpicked selection</p>
+            </div>
+            {/* <h2 className="text-xl text-center font-lexend-deca">Featured Products</h2> */}
+            <Link href="/" className="hidden md:inline-flex items-center gap-2 px-6 py-3 bg-gray-100 hover:bg-gray-200 rounded-full font-lexend-deca transition-all duration-300">Explore More
+              <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 12H20M20 12L14 6M20 12L14 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+              </svg>
+            </Link>
+            
           </div>
           <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
               <div className="bg-white h-[650px] rounded-3xl shadow-lg border border-gray-200">
-                <div className="relative mx-auto w-[90%] mt-[5%] h-[60%] bg-[url('https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/395016/04/sv01/fnd/IDN/fmt/png/Sepatu-Sneaker-Unisex-Puma-Caven-2.0-Lux')] bg-cover bg-center rounded-2xl border border-gray-200"></div>
-                <p className="product-brand mt-3 font-lexend-deca text-white text-1xl inline-flex items-center gap-2 px-[30px] py-2 rounded-r-lg bg-orange-500 shadow-lg">Unisex</p>
-                <div className="p-6 space-y-3">
-                  <p className="product-title font-lexend-deca text-1xl">Sepatu Sneaker Unisex Puma Caven 2.0 Lux</p>
+                <div className="relative mx-auto w-[90%] mt-[5%] h-[60%] bg-[url('https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/395016/04/sv01/fnd/IDN/fmt/png/Sepatu-Sneaker-Unisex-Puma-Caven-2.0-Lux')] bg-contain bg-no-repeat bg-center rounded-2xl"></div>
+                <p className="product-brand mt-3 font-lexend-deca text-white text-1xl inline-flex items-center gap-2 px-[30px] py-2 rounded-r-lg bg-[#A8DF8E] shadow-lg">Unisex</p>
+                <div className="p-6 space-y-3 flex flex-col flex-grow">
+                  <p className="product-title font-lexend-deca text-1xl line-clamp-2 min-h-[45px]">Sepatu Sneaker Unisex Puma Caven 2.0 Lux</p>
                   <p className="product-title font-lexend-deca text-1xl font-semibold">Rp. 25.000</p>
-                    <Link href="/" target="blank" className="block w-full py-3 text-center rounded-2xl bg-black">
+                    <Link href="/" target="blank" className="block w-full py-3 text-center rounded-2xl bg-[#1B211A]">
                       <p className="font-lexend-deca text-white text-[20px]">Buy Now</p>
                     </Link>
                 </div>
               </div>
+              
               <div className="bg-white h-[650px] rounded-3xl shadow-lg border border-gray-200">
-                <div className="relative mx-auto w-[90%] mt-[5%] h-[60%] bg-[url('https://cdnpro.eraspace.com/media/catalog/product/a/p/apple_iphone_15_blue_1.jpg')] bg-cover bg-center rounded-2xl border border-gray-200"></div>
-                <p className="product-brand mt-3 font-lexend-deca text-white text-1xl inline-flex items-center gap-2 px-[30px] py-2 rounded-r-lg bg-orange-500 shadow-lg">Apple</p>
-                <div className="p-6 space-y-3">
-                  <p className="product-title font-lexend-deca text-1xl">Iphone 15</p>
+                <div className="relative mx-auto w-[90%] mt-[5%] h-[60%] bg-[url('https://cdnpro.eraspace.com/media/catalog/product/a/p/apple_iphone_15_blue_1.jpg')] bg-contain bg-no-repeat bg-center rounded-2xl"></div>
+                <p className="product-brand mt-3 font-lexend-deca text-white text-1xl inline-flex items-center gap-2 px-[30px] py-2 rounded-r-lg bg-[#A8DF8E] shadow-lg">Apple</p>
+                <div className="p-6 space-y-3 flex flex-col flex-grow">
+                  <p className="product-title font-lexend-deca text-1xl line-clamp-2 min-h-[45px]">Iphone 15</p>
                   <p className="product-title font-lexend-deca text-1xl font-semibold">Rp. 11.249.000</p>
-                    <Link href="/" target="blank" className="block w-full py-3 text-center rounded-2xl bg-black">
+                    <Link href="/" target="blank" className="block w-full py-3 text-center rounded-2xl bg-[#1B211A]">
                       <p className="font-lexend-deca text-white text-[20px]">Buy Now</p>
                     </Link>
                 </div>
               </div>
-               <div className="bg-white h-[650px] rounded-3xl shadow-lg border border-gray-200">
-                <div className="relative mx-auto w-[90%] mt-[5%] h-[60%] bg-[url('https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/395016/04/sv01/fnd/IDN/fmt/png/Sepatu-Sneaker-Unisex-Puma-Caven-2.0-Lux')] bg-cover bg-center rounded-2xl border border-gray-200"></div>
-                <p className="product-brand mt-3 font-lexend-deca text-white text-1xl inline-flex items-center gap-2 px-[30px] py-2 rounded-r-lg bg-orange-500 shadow-lg">Unisex</p>
-                <div className="p-6 space-y-3">
-                  <p className="product-title font-lexend-deca text-1xl">Sepatu Sneaker Unisex Puma Caven 2.0 Lux</p>
-                  <p className="product-title font-lexend-deca text-1xl font-semibold">Rp. 25.000</p>
-                    <Link href="/" target="blank" className="block w-full py-3 text-center rounded-2xl bg-black">
-                      <p className="font-lexend-deca text-white text-[20px]">Buy Now</p>
-                    </Link>
-                </div>
-              </div>
-              <div className="bg-white h-[650px] rounded-3xl shadow-lg border border-gray-200">
-                <div className="relative mx-auto w-[90%] mt-[5%] h-[60%] bg-[url('https://cdnpro.eraspace.com/media/catalog/product/a/p/apple_iphone_15_blue_1.jpg')] bg-cover bg-center rounded-2xl border border-gray-200"></div>
-                <p className="product-brand mt-3 font-lexend-deca text-white text-1xl inline-flex items-center gap-2 px-[30px] py-2 rounded-r-lg bg-orange-500 shadow-lg">Apple</p>
-                <div className="p-6 space-y-3">
-                  <p className="product-title font-lexend-deca text-1xl">Iphone 15</p>
-                  <p className="product-title font-lexend-deca text-1xl font-semibold">Rp. 11.249.000</p>
-                    <Link href="/" target="blank" className="block w-full py-3 text-center rounded-2xl bg-black">
-                      <p className="font-lexend-deca text-white text-[20px]">Buy Now</p>
-                    </Link>
-                </div>
-              </div>
+              
+              
           </div>
         </div>
-        <Link href="/" target="blank" className="md:invisible flex justify-center mt-10 md:mt-0">
-            <span className="inline-flex items-center justify-center px-8 py-3 bg-[#f97316] rounded-lg text-white font-lexend-deca">Explore More</span>
-        </Link>
-        <div className="h-[500px] bg-black mt-12">
-
+        <div className="flex justify-center mt-10 md:hidden">
+          <Link href="/" className="inline-flex items-center gap-2 px-8 py-3 bg-gray-100 hover:bg-gray-200 rounded-full text-black font-lexend-deca transition-all duration-300 shadow-lg">
+            Explore More
+            <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M4 12H20M20 12L14 6M20 12L14 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+            </svg>
+          </Link>
         </div>
-        <div className="h-[500px] mt-12">
+        <div className="relative h-[500px] bg-[#0a0a0a] rounded-b-4xl mt-12">
+          <p className="text-white">How Potion
+works</p>
+        </div>
+        <div className="relative h-[500px]">
 
         </div>
         
@@ -118,66 +114,4 @@ export default async function Home() {
   ); 
 }
 
-  // return (
-  //   <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-  //     <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-  //       <Image
-  //         className="dark:invert"
-  //         src="/next.svg"
-  //         alt="Next.js logo"
-  //         width={100}
-  //         height={20}
-  //         priority
-  //       />
-  //       <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-  //         <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-  //           To get started, edit the page.tsx file.
-  //         </h1>
-  //         <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-  //           Looking for a starting point or more instructions? Head over to{" "}
-  //           <a
-  //             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-  //             className="font-medium text-zinc-950 dark:text-zinc-50"
-  //           >
-  //             Templates
-  //           </a>{" "}
-  //           or the{" "}
-  //           <a
-  //             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-  //             className="font-medium text-zinc-950 dark:text-zinc-50"
-  //           >
-  //             Learning
-  //           </a>{" "}
-  //           center.
-  //         </p>
-  //       </div>
-  //       <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-  //         <a
-  //           className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-  //           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //         >
-  //           <Image
-  //             className="dark:invert"
-  //             src="/vercel.svg"
-  //             alt="Vercel logomark"
-  //             width={16}
-  //             height={16}
-  //           />
-  //           Deploy Now
-  //         </a>
-  //         <a
-  //           className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-  //           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //         >
-  //           Documentation
-  //         </a>
-  //       </div>
-  //     </main>
-  //   </div>
-  // );
-  
 
